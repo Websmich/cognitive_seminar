598dc2d84bafce4a396624bb5edf50893c1d27ca
Michael Webster <michaeldwebster@outlook.com>
2018-04-18 17:39:37 -0400

    fixed project

---
+++ SeminarApp/SeminarApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SeminarApp.Models;
+
+namespace SeminarApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Translate(String translateWord)
+        {
+            Console.WriteLine(translateWord);
+            return View();
+        }
+
+    }
+}
